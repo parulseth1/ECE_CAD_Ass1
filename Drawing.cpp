05b45521c7bece8467fa4f29ceac352beabc0263
Aliakbar Eski <theeskis@gmail.com>
2015-10-03 17:40:05 -0400

    Grpahics wip

--- Drawing.cpp
+++ Drawing.cpp
@@ -10,8 +10,8 @@ int DrawNow()
     init_graphics("Drawing1", WHITE);
     int a;
     std::cin>>a;
-    set_visible_world(0, 0, 1000, 1000);
-    update_message ( " Interactive graphics example . " ) ;
+    set_visible_world(0, 1000, 1000, 0);
+    update_message ( " MAZE ROUTER" ) ;
     
     
     close_graphics () ;
